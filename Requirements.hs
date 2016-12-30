bc2a15917165125a8cf8b37f98416e65c12ac7ac
P. S. Clarke <rota2@pscl4rke.net>
2016-12-30 15:29:21 +0000

    Increase size of default requirements...
    The processing time increases dramatically as number of counters
    increases.  This increase makes it more obvious.

--- Requirements.hs
+++ Requirements.hs
@@ -6,9 +6,15 @@ import Planning
 counters =
     [ Counter "Alice"
     , Counter "Bob"
+    , Counter "Carol"
+    , Counter "Dave"
+    , Counter "Eve"
     ]
 
 slots =
     [ Slot 2 "1st Jan"
     , Slot 1 "2nd Jan"
+    , Slot 2 "3rd Jan"
+    , Slot 2 "4th Jan"
+    , Slot 3 "5th Jan"
     ]
