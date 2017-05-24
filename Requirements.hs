6814f068429da6d83ca165b08c161fae5fe8b3be
P. S. Clarke <rota2@pscl4rke.net>
2017-05-24 23:24:29 +0100

    DSL-style functions for defining requirements.

--- Requirements.hs
+++ Requirements.hs
@@ -3,6 +3,18 @@ module Requirements where
 
 import Planning
 
+
+
+allExcept :: [Counter] -> Preferences
+allExcept cs = Preferences [(c, CannotDo) | c <- cs]
+
+without :: [Counter] -> [Counter] -> Preferences
+without musts cannots = Preferences (mustpairs ++ cannotpairs)
+  where mustpairs = [(c, MustDo) | c <- musts]
+        cannotpairs = [(c, CannotDo) | c <- cannots]
+
+
+
 alice =     Counter "Alice"
 bob =       Counter "Bob"
 carol =     Counter "Carol"
@@ -18,10 +30,10 @@ counters =
     ]
 
 slots =
-    [ Slot 2 "1st Jan" (Preferences [(carol, MustDo)])
+    [ Slot 2 "1st Jan" ([carol] `without` [])
     , Slot 1 "2nd Jan" allFree
     , Slot 2 "3rd Jan" allFree
-    , Slot 2 "4th Jan" allFree
+    , Slot 2 "4th Jan" (allExcept [dave, eve])
     , Slot 3 "5th Jan" allFree
     , Slot 2 "6th Jan" allFree
     , Slot 2 "7th Jan" allFree
