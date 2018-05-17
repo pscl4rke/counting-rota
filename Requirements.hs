e7530b9bb7e3bf6680130f4d0c14a25fbfaaf37b
P. S. Clarke <rota2@pscl4rke.net>
2018-05-17 22:48:42 +0100

    Utility function for slightly tabulated stuff.

--- Requirements.hs
+++ Requirements.hs
@@ -1,6 +1,8 @@
 
 module Requirements where
 
+import Data.List.Split (splitOn)
+
 import Parsing
 import Planning
 
@@ -30,12 +32,17 @@ p names = case (parseListOfUnsureAboutCounter counters names) of
     Right unsurecounters -> unsurecounters
     Left msg -> error ("Cannot parse availability: " ++ msg)
 
+r :: String -> Preferences
+r line = case (splitOn "|" line) of
+    [left, right] -> ((q left) `without` (p right))
+    _ -> error ("Invalid columns: '" ++ line ++ "'")
+
 slots =
-    [ Slot 2 "1st Jan" (q "Carol" `without` p "")
-    , Slot 1 "2nd Jan" allFree
-    , Slot 2 "3rd Jan" allFree
-    , Slot 2 "4th Jan" (q "" `without` p "Dave, Eve")
-    , Slot 3 "5th Jan" (q "" `without` p "  Alice")
-    , Slot 2 "6th Jan" (q "   " `without` p "Bob?, Dave")
-    , Slot 2 "7th Jan" allFree
+    [ Slot 2 "1st Jan" (r "Carol    |               ")
+    , Slot 1 "2nd Jan" (r "         |               ")
+    , Slot 2 "3rd Jan" (r "         |               ")
+    , Slot 2 "4th Jan" (r "         | Dave, Eve     ")
+    , Slot 3 "5th Jan" (r "         | Alice         ")
+    , Slot 2 "6th Jan" (r "         | Bob?, Dave    ")
+    , Slot 2 "7th Jan" (r "         |               ")
     ]
